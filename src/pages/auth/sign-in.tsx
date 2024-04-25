62ce8b802f36ca5db9d390ece2938e1eb85b7aa0
Cássia <cascrisbern@gmail.com>
2024-04-25 13:16:00 +0300

    fix on the text-align of the labels added sign-up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -35,6 +36,9 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button asChild className="absolute right-8 top-8" variant="ghost">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -45,7 +49,7 @@ export function SignIn() {
             </p>
 
             <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
-              <div className="space-y-2">
+              <div className="space-y-2 text-left">
                 <Label htmlFor="email">Seu e-mail</Label>
                 <Input id="email" type="email" {...register('email')} />
               </div>
