0d722801fc7de676964d2cdf323c1d7aea2c81e5
Cássia <cascrisbern@gmail.com>
2024-05-01 12:12:35 +0300

    added filter for the orders

--- src/pages/app/orders/order.tsx
+++ src/pages/app/orders/order.tsx
@@ -1,15 +1,15 @@
-import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { Button } from '@/components/ui/button'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
   return (
@@ -20,14 +20,7 @@ export function Orders() {
       </div>
 
       <div className="space-y-2.5">
-        <form action="#" className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <input
-            type="text"
-            placeholder="Nome do cliente"
-            className="h-8 w-[320px]"
-          />
-        </form>
+        <OrderTableFilters />
 
         <div className="rounded-md border">
           <Table>
@@ -46,51 +39,7 @@ export function Orders() {
 
             <TableBody>
               {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <TableRow key={i}>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <Search className="h-3 w-3" />
-                        <span className="sr-only">Detalhes do pedido</span>
-                      </Button>
-                    </TableCell>
-
-                    <TableCell className="font-mono text-xs font-medium">
-                      jfhgjfo
-                    </TableCell>
-
-                    <TableCell className="text-muted-foreground">
-                      há 15 minutos
-                    </TableCell>
-
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400" />
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-
-                    <TableCell className="font-medium">Ana Maria</TableCell>
-
-                    <TableCell className="font-medium">R$ 149,00</TableCell>
-
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <ArrowRight className="mr-22 h-3 w-3" />
-                        Aprovar
-                      </Button>
-                    </TableCell>
-
-                    <TableCell>
-                      <Button variant="ghost" size="xs">
-                        <X className="mr-22 h-3 w-3" />
-                        Cancelar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                )
+                return <OrderTableRow key={i} />
               })}
             </TableBody>
           </Table>
