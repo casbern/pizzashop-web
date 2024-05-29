9cae03a60b91f1ebf721be7cdc76baec45aec765
Cássia <cascrisbern@gmail.com>
2024-05-29 14:51:49 +0300

    added skeleton for the graphs and table added costumized error page

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -15,6 +15,7 @@ import {
 
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -77,17 +78,18 @@ export function Orders() {
                     return <OrderTableRow key={order.orderId} order={order} />
                   })}
               </TableBody>
+              {isLoadingOrders && <OrderTableSkeleton />}
+
+              {result && (
+                <Pagination
+                  pageIndex={result.meta.pageIndex}
+                  totalCount={result.meta.totalCount}
+                  perPage={result.meta.perPage}
+                  onPageChange={handlePaginate}
+                />
+              )}
             </Table>
           </div>
-
-          {result && (
-            <Pagination
-              pageIndex={result.meta.pageIndex}
-              totalCount={result.meta.totalCount}
-              perPage={result.meta.perPage}
-              onPageChange={handlePaginate}
-            />
-          )}
         </div>
       </div>
     </>
